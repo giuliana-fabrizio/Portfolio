a040aa912d6f40392bdeb402bcb9c2700fbec3c2
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-06-05 13:10:23 +0200

    chore: minor changes

--- portfolio/src/views/ProjectsView.vue
+++ portfolio/src/views/ProjectsView.vue
@@ -9,14 +9,14 @@
         <main id="projects_main" class="py-5">
             <div class="container">
                 <div class="row">
-                    <div v-for="(project, key) in projects" :key="key" class="col-md-4 mb-4">
+                    <div v-for="(project, key) in projects" :key="key" :class="[getClass(Object.keys(projects).length), 'mb-4']">
                         <div class="card project-card">
                             <div class="card-body m-3">
                                 <div class="d-flex justify-content-between mb-3">
                                     <h5>{{ project.title }}</h5>
                                     <p class="badge bg-primary text-wrap">{{ project.category }}</p>
                                 </div>
-                                <p class="mb-3">{{ truncatedInstructions(project.instructions) }}</p>
+                                <p class="mb-3" v-html="formattedText(truncatedInstructions(project.instructions.text))"></p>
                                 <div class="d-flex flex-wrap">
                                     <span
                                         v-for="(technologie, index) in project.technologies"
@@ -39,7 +39,7 @@ export default {
     name: 'ProjectsView',
 
     data: () => ({
-        colors: ["bg-success", "bg-danger", "bg-secondary", "bg-warning", "bg-info", "bg-light", "bg-dark"],
+        colors: ["bg-success", "bg-danger", "bg-warning", "bg-info", "bg-secondary", "bg-dark"],
     }),
 
     props: {
@@ -49,11 +49,23 @@ export default {
 
     methods: {
         truncatedInstructions(instructions) {
-            const maxLength = 100;
+            const maxLength = Object.keys(this.projects).length <= 2 ? 175 : 100;
             if (instructions.length > maxLength) {
                 return instructions.substring(0, maxLength) + '...';
             }
             return instructions;
+        },
+        formattedText(text) {
+            if (!text) return '';
+            return text
+                .replace(/\n/g, '<br>')
+                .replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;');
+        },
+        getClass(length) {
+            if (length <= 2) {
+                return 'col-md-6'; // Prend la moitié de la page
+            }
+            return 'col-md-4'; // Taille par défaut pour 3 projets ou plus
         }
     }
 }
