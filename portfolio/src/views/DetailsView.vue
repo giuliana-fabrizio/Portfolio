be9f82238a6ca6298e13b5f8e37fb9cf1096b25d
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-06-06 00:50:39 +0200

    feat: update details page

--- portfolio/src/views/DetailsView.vue
+++ portfolio/src/views/DetailsView.vue
@@ -1,27 +1,52 @@
 <template>
     <div>
-        <header id="projects_header" class="text-center">
+        <header id="projects_header">
             <section id="titre" class="w-100">
                 <h1 class="mt-5 text-primary">{{ elem.title }}</h1>
             </section>
         </header>
-  
-        <main id="projects_main" class="py-5">
-            <div class="container">
 
+        <main id="projects_main" class="d-flex justify-content-center pb-5 pt-5">
+            <div class="col-md-8">
+                <div class="row position-relative">
+                    <div v-for="(section, key) in elem" :key="key" class="col-12">
+
+                        <section v-if="section.title && section.text" class="p-5 pb-0 pt-0">
+                                <h2 class="text-dark mb-5">{{ section.title }}</h2>
+
+                                <p v-html="formattedText(section.text)" class="text-xl-start text-secondary mt-3"></p>
+
+                                <div v-for="(image, index) in section.images" :key="index" class="row justify-content-center">
+                                    <div class="col-12 col-md-8">
+                                        <img :src="image.path" :alt="image.alt" class="img-fluid img-page-details mt-5">
+                                        <p class="text-muted text-center mt-2 fst-italic">{{ image.legend }}</p>
+                                    </div>
+                                </div>
+
+                            <hr class="mb-5 mt-5">
+                        </section>
+                    </div>
+                </div>
             </div>
         </main>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'ProjectsView',
+    export default {
+        name: 'ProjectsView',
 
-    props: {
-        elem: Object
-    },
+        props: {
+            elem: Object
+        },
 
-    methods: {}
-}
+        methods: {
+            formattedText(text) {
+                if (!text) return '';
+                return text
+                    .replace(/\n/g, '<br>')
+                    .replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;');
+            },
+        }
+    }
 </script>
