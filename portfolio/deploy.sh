96f07f7c9a9835609dea615282dc856af27d6466
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-06-05 13:03:57 +0200

    chore: minor change

--- portfolio/deploy.sh
+++ portfolio/deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:giuliana-fabrizio/Portfolio.git main:main
+git push -f git@github.com:giuliana-fabrizio/Portfolio.git main:prod
 
 cd -
