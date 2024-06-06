d24c061f40d9a851bc9fa463bc0f22e1e10f1af1
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-06-06 15:00:25 +0200

    style: use bootstrap

--- portfolio/src/views/AllProjectsView.vue
+++ portfolio/src/views/AllProjectsView.vue
@@ -6,20 +6,22 @@
             </section>
         </header>
 
-        <main id="all_projects_main">
-            <div>
+        <main id="all_projects_main" class="d-flex justify-content-center p-3 pb-5 pt-5">
+            <div class="col-md-7">
                 <div v-for="(experience, key) in experiences" :key="key">
                     <h3 class="heading">{{ experience.title }}</h3>
-                    <div v-for="(year, index) in experience.years" :key="index" class="container projects">
-                        <div class="project-category-image">
-                            <img src="images/case.png" class="project-category-image" />
-                        </div>
-                        <div class="project-category-info">
-                            <h5>{{ year.category }}</h5>
-                            <p class="display-summary">{{ year.summary }}</p>
-                            <router-link :to="{ name: 'projects' }" class="btn btn-primary mt-2">
-                                <div @click="setProjects(year)">Voir les projets</div>
-                            </router-link>
+                    <div v-for="(year, index) in experience.years" :key="index" class="container mb-4 p-3 bg-white rounded shadow">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col-4 d-none d-md-flex align-items-center">
+                                <img src="images/case.png" class="img-fluid" style="padding: 15px; width: 200px;">
+                            </div>
+                            <div class="col-8">
+                                <h5 class="text-dark mb-3" style="font-size: 1.3em;">{{ year.category }}</h5>
+                                <p class="text-secondary" style="font-size: 0.9em;">{{ year.summary }}</p>
+                                <router-link :to="{ name: 'projects' }" class="btn btn-primary mt-2" @click="setProjects(year)">
+                                    Voir les projets
+                                </router-link>
+                            </div>
                         </div>
                     </div>
                 </div>
