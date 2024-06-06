95965dfde06a927e57f32528fe81d618840a3b5a
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-06-06 16:17:07 +0200

    style: use bootstrap

--- portfolio/src/views/ProjectsView.vue
+++ portfolio/src/views/ProjectsView.vue
@@ -1,24 +1,23 @@
 <template>
-    <div>
+    <div class="m-2">
         <header id="projects_header" class="text-center">
             <section id="titre" class="w-100">
                 <h1 class="mt-5 text-primary">{{ data.category }}</h1>
             </section>
         </header>
   
-        <main id="projects_main" class="m-3 py-5">
+        <main id="projects_main" class="py-5">
             <div class="container">
                 <div class="row">
                     <div v-for="(project, key) in data.projects" :key="key" :class="[getClass(Object.keys(data.projects).length), 'mb-4']">
                         <router-link :to="{ name: 'details' }" class="text-decoration-none">
-                            <div class="card project-card" @click="setProject(project)">
+                            <div class="card bg-light h-100 shadow-sm border-0 rounded" @click="setProject(project)" style="background-color: #f4f4f9; transition: transform 0.3s, box-shadow 0.3s;">
                                 <div class="card-body m-3">
-                                    <div class="custom-card-header d-flex justify-content-between align-items-center mb-3">
-                                        <h5><strong>{{ project.title }}</strong></h5>
+                                    <div class="d-flex justify-content-between align-items-center mb-3 flex-column flex-md-row text-center text-md-start">
+                                        <h5 style="color:#555"><strong>{{ project.title }}</strong></h5>
                                         <p class="badge bg-primary text-wrap">{{ project.category }}</p>
                                     </div>
-
-                                    <p class="custom-text-size mb-3" v-html="formattedText(project.technologies ? truncatedInstructions(project.instructions.text) : project.instructions.text)"></p>
+                                    <p class="text-secondary mb-3 d-xl-block" v-html="formattedText(project.technologies ? truncatedInstructions(project.instructions.text) : project.instructions.text)"></p>
                                     <div class="d-flex flex-wrap">
                                         <span
                                             v-for="(technologie, index) in project.technologies"
@@ -69,7 +68,7 @@ export default {
             if (length <= 2) {
                 return 'col-md-6';
             }
-            return 'col-md-4';
+            return 'col-12 col-sm-6 col-md-4';
         },
         setProject(project) {
             this.$store.commit('setProject', project);
@@ -79,16 +78,8 @@ export default {
 </script>
 
 <style>
-@media (max-width: 1200px) {
-    .custom-text-size {
-        display: none !important;
-    }
-}
-
-@media (max-width: 1000px) {
-    .custom-card-header {
-        display: block !important;
-        text-align: center;
-    }
+.card:hover {
+    transform: translateY(-10px);
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
 }
 </style>
