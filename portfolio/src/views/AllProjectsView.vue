0dcc57145d015c7247c3b0d5e5732725efacc8a3
Giuliana FABRIZIO <godailfabriziogiuliana@gmail.com>
2024-09-15 15:33:48 +0200

    feat: update the design of the projects page

--- portfolio/src/views/AllProjectsView.vue
+++ portfolio/src/views/AllProjectsView.vue
@@ -2,27 +2,34 @@
     <div>
         <header id="all_projects_header">
             <section id="titre" class="w-100">
-                <h1 class="mt-5 text-primary">{{ title }}</h1>
+                <h1 class="mb-4 mt-5 text-primary">{{ title }}</h1>
+                <p class="text-secondary">{{introText}}</p>
             </section>
         </header>
 
-        <main id="all_projects_main" class="d-flex justify-content-center p-3 pb-5 pt-5">
-            <div class="col-md-7">
-                <div v-for="(experience, key) in experiences" :key="key">
-                    <h3 class="heading">{{ experience.title }}</h3>
-                    <div v-for="(year, index) in experience.years" :key="index" class="container mb-4 p-3 bg-white rounded shadow">
-                        <div class="row d-flex justify-content-center">
-                            <div class="col-4 d-none d-md-flex align-items-center">
-                                <img src="images/case.png" class="img-fluid" style="padding: 15px; width: 200px;">
+        <main id="all_projects_main" class="d-flex justify-content-center pb-5">
+            <div class="container">
+                <div class="row">
+                    <div v-for="(project, key) in projects" :key="key" :class="[getClass(Object.keys(projects).length), 'mb-4']">
+                        <router-link :to="{ name: 'details' }" class="text-decoration-none">
+                            <div class="card bg-light h-100 shadow-sm border-0 rounded" @click="setProject(project)" style="background-color: #f4f4f9; transition: transform 0.3s, box-shadow 0.3s;">
+                                <div class="card-body m-3">
+                                    <div class="d-flex justify-content-between align-items-center mb-3 flex-column flex-md-row text-center text-md-start">
+                                        <h5 style="color:#555"><strong>{{ project.title }}</strong></h5>
+                                        <p class="badge bg-primary text-wrap">{{ project.category }}</p>
+                                    </div>
+                                    <p class="text-secondary mb-3 d-xl-block" v-html="formattedText(project.technologies ? truncatedInstructions(project.instructions.text) : project.instructions.text)"></p>
+                                    <div class="d-flex flex-wrap">
+                                        <span
+                                            v-for="(technologie, index) in project.technologies"
+                                            :key="index"
+                                            :class="[technologie.class, 'badge', 'text-wrap', 'me-2', 'mb-2']">
+                                            {{ technologie.name }}
+                                        </span>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-8">
-                                <h5 class="text-dark mb-3" style="font-size: 1.3em;">{{ year.category }}</h5>
-                                <p class="text-secondary" style="font-size: 0.9em;">{{ year.summary }}</p>
-                                <router-link :to="{ name: 'projects' }" class="btn btn-primary mt-2">
-                                    <div @click="setProjects(year)">{{ see_projects }}</div>
-                                </router-link>
-                            </div>
-                        </div>
+                        </router-link>
                     </div>
                 </div>
             </div>
@@ -46,8 +53,8 @@ export default {
 
     data: () => ({
         title: "",
-        experiences: {},
-        see_projects: ""
+        introText: "",
+        projects: {},
     }),
 
     created() {
@@ -55,22 +62,33 @@ export default {
         const isFrench = language === 'french';
 
         this.title = isFrench ? variables_fr.projects_page_title : variables_en.projects_page_title;
-        this.experiences = isFrench ? variables_fr.experiences : variables_en.experiences;
-        this.see_projects = isFrench ? variables_fr.more_projects : variables_en.more_projects;
+        this.introText = isFrench ? variables_fr.projects_page_presentation : variables_en.projects_page_presentation;
+        this.projects = isFrench ? variables_fr.projects : variables_en.projects;
     },
 
     methods: {
-        setProjects(projects) {
-            this.$store.commit('setProjects', projects);
+        truncatedInstructions(instructions) {
+            const maxLength = Object.keys(this.projects).length <= 2 ? 175 : 100;
+            if (instructions.length > maxLength) {
+                return instructions.substring(0, maxLength) + '...';
+            }
+            return instructions;
+        },
+        formattedText(text) {
+            if (!text) return '';
+            return text
+                .replace(/\n/g, '<br>')
+                .replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;');
+        },
+        getClass(length) {
+            if (length <= 2) {
+                return 'col-md-6';
+            }
+            return 'col-12 col-sm-6 col-md-4';
+        },
+        setProject(project) {
+            this.$store.commit('setProject', project);
         }
     }
 }
 </script>
-
-<style>
-@media (max-width: 770px) {
-    .display-summary {
-        display: none !important;
-    }
-}
-</style>
