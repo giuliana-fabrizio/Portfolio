598397821547f69e84dad8b5ba5e6352e99d22e0
Giuliana FABRIZIO <godailfabriziogiuliana@gmail.com>
2024-09-22 19:06:04 +0200

    chore: minor change

--- portfolio/src/components/NavBarComponent.vue
+++ portfolio/src/components/NavBarComponent.vue
@@ -1,29 +1,29 @@
 <template>
     <nav class="container d-flex justify-content-between align-items-center py-3">
-            <img src="images/logo.png" style="width: 27px;">
+        <img src="images/logo.png" style="width: 27px;">
 
-            <ul class="d-flex">
-                <li>
-                    <router-link to="/">{{ home_btn }}</router-link>
-                </li>
-                <li>
-                    <router-link to="/academic_training">{{ academic_training }}</router-link>
-                </li>
-                <li>
-                    <router-link to="/all_projects">{{ projects_btn }}</router-link>
-                </li>
-                <li v-if="canChangeLanguage" :class="language === 'french' && 'd-none'">
-                    <button class="btn p-0" @click="changeLanguage('french')">
-                        <img src="images/france.png"  style="width: 27px;">
-                    </button>
-                </li>
-                <li v-if="canChangeLanguage" :class="language === 'english' && 'd-none'">
-                    <button class="btn p-0" @click="changeLanguage('english')">
-                        <img src="images/royaume-uni.png" style="width: 27px;">
-                    </button>
-                </li>
-            </ul>
-        </nav>
+        <ul class="d-flex">
+            <li>
+                <router-link to="/">{{ home_btn }}</router-link>
+            </li>
+            <li>
+                <router-link to="/academic_training">{{ academic_training }}</router-link>
+            </li>
+            <li>
+                <router-link to="/all_projects">{{ projects_btn }}</router-link>
+            </li>
+            <li v-if="canChangeLanguage" :class="language === 'french' && 'd-none'">
+                <button class="btn p-0" @click="changeLanguage('french')">
+                    <img src="images/france.png"  style="width: 27px;">
+                </button>
+            </li>
+            <li v-if="canChangeLanguage" :class="language === 'english' && 'd-none'">
+                <button class="btn p-0" @click="changeLanguage('english')">
+                    <img src="images/royaume-uni.png" style="width: 27px;">
+                </button>
+            </li>
+        </ul>
+    </nav>
 </template>
 
 <script>
