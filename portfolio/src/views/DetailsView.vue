574990524d91cd1de79a66fb4c187f0d5cb8039b
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-11-16 00:05:32 +0100

    feat: update projet details diplay

--- portfolio/src/views/DetailsView.vue
+++ portfolio/src/views/DetailsView.vue
@@ -9,18 +9,26 @@
         <main id="projects_main" class="d-flex justify-content-center pb-5 pt-2">
             <div class="col-md-8">
                 <div class="row position-relative">
-                    <div v-for="(section, key, id) in project" :key="key" class="col-12">
-                        <section v-if="section.title && section.text" class="p-5 pb-0 pt-0">
-                            <h2 class="text-dark mb-5">{{ section.title }}</h2>
-                            <p v-html="formattedText(section.text)" class="text-xl-start text-secondary mt-3"></p>
-                            <div v-for="(image, index) in section.images" :key="index" class="row justify-content-center">
-                                <div class="col-12 col-md-9">
-                                    <img :src="image.path" :alt="image.alt" class="img-fluid img-page-details mt-5">
-                                    <p class="text-muted text-center mt-2 fst-italic">{{ image.legend }}</p>
-                                </div>
+                    <div v-for="(content, key, id) in project" :key="key" class="col-12">
+                        <hr v-if="id > 2" class="mb-5 mt-5">
+                        <div v-if="id > 1">
+                            <div v-for="(section, skey) in content" :key="skey">
+                                <section v-if="typeof skey == String('string')" class="p-5 pb-0 pt-0">
+                                    <div v-if="skey.substring(0, 6) != String('images')">
+                                        <h2 v-if="skey == String('title')" class="text-dark mb-5">{{ section }}</h2>
+                                        <p v-else v-html="formattedText(String(section))" class="text-xl-start text-secondary mt-3"></p>
+                                    </div>
+                                    <div v-else>
+                                        <div v-for="(image, index) in section" :key="index" class="row justify-content-center">
+                                            <div class="col-12 col-md-9">
+                                                <img :src="image.path" :alt="image.alt" class="img-fluid img-page-details mt-5">
+                                                <p class="text-muted text-center mt-2 fst-italic">{{ image.legend }}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
                             </div>
-                            <hr v-if="id < project_length" class="mb-5 mt-5">
-                        </section>
+                        </div>
                     </div>
                 </div>
             </div>
