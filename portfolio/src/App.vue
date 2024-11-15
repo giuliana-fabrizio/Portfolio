1e02f499fbf3bde6ef11f48a45348e2165effa25
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-11-15 13:22:25 +0100

    feat: move footer in a component

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -7,64 +7,19 @@
     <router-view />
 
     <footer class="mt-auto">
-        <section id="contact" class="container social">
-            <div class="row">
-                <div class="col-12 text-center">
-                    <ul class="list-inline">
-                        <li class="list-inline-item">
-                            <a :href="mail" target="_blank">
-                                <i class="bi bi-envelope-fill fs-4"></i>
-                            </a>
-                        </li>
-
-                        <li class="list-inline-item">
-                            <a :href="github" target="_blank">
-                                <i class="bi bi-github fs-4"></i>
-                            </a>
-                        </li>
-
-                        <li class="list-inline-item">
-                            <a :href="linkedin" target="_blank">
-                                <i class="bi bi-linkedin fs-4"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-12 text-center pt-2">
-                    <p class="copyright">
-                        &copy; 2024 - {{ name }}
-                    </p>
-                </div>
-            </div>
-        </section>
+        <FooterComponent />
     </footer>
 </div>
 </template>
 
 <script>
 import NavBarComponent from './components/NavBarComponent.vue';
-import variables from './variables_fr.js';
+import FooterComponent from './components/FooterComponent.vue';
 
 export default {
     components: {
-        NavBarComponent
-    },
-
-    data: () => ({
-        name: "",
-        mail: "",
-        github: "",
-        linkedin: "",
-    }),
-
-    created() {
-        this.name = variables.name;
-        this.mail = variables.mail;
-        this.github = variables.github;
-        this.linkedin = variables.linkedin;
+        NavBarComponent,
+        FooterComponent
     }
 }
 </script>
---
+++ portfolio/src/components/FooterComponent.vue
@@ -0,0 +1,57 @@
+<template>
+    <section id="contact" class="container social">
+        <div class="row">
+            <div class="col-12 text-center">
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <a :href="mail" target="_blank">
+                            <i class="bi bi-envelope-fill fs-4"></i>
+                        </a>
+                    </li>
+
+                    <li class="list-inline-item">
+                        <a :href="github" target="_blank">
+                            <i class="bi bi-github fs-4"></i>
+                        </a>
+                    </li>
+
+                    <li class="list-inline-item">
+                        <a :href="linkedin" target="_blank">
+                            <i class="bi bi-linkedin fs-4"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 text-center pt-2">
+                <p class="copyright">
+                    &copy; 2024 - {{ name }}
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import variables from '../variables_fr.js';
+
+export default {
+    name: "FooterComponent",
+
+    data: () => ({
+        name: "",
+        mail: "",
+        github: "",
+        linkedin: "",
+    }),
+
+    created() {
+        this.name = variables.name;
+        this.mail = variables.mail;
+        this.github = variables.github;
+        this.linkedin = variables.linkedin;
+    }
+}
+</script>
