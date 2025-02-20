814d7ea0a50156995fd08789f02a7607f1274b36
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-02-20 16:11:06 +0100

    feat: can flip the card

--- portfolio/src/components/DegreeSectionComponent.vue
+++ portfolio/src/components/DegreeSectionComponent.vue
@@ -2,14 +2,41 @@
     <section :class="[margin ? '' : 'mt-md-0', 'mt-5', 'row']">
         <h5><strong class="subtitle">{{ title }}</strong></h5>
         <div v-for="(item, key) in items" :key="key" class="col-6 col-lg-4 mt-3">
-            <div class="card h-100 p-3">
-                <div class="d-flex align-items-center flex-column justify-content-center">
-                    <img
-                        :src="item.logo"
-                        alt="Logo"
-                        class="img-fluid mb-1"
-                        style="max-height: 125px;">
-                    <p class="mb-0">{{ item.title }}</p>
+            <div @click="toggleFlip(key)" class="card p-1" role="button" style="height: 200px;">
+                <div :class="[{ flipped: flippedCards[key] }, 'flip-inner', 'my-auto']">
+
+                    <!-- Front -->
+                    <div :class="[flippedCards[key] ? 'd-none' : 'd-block', 'mb-0', 'flip-front']">
+                        <img :src="item.logo" alt="Logo" class="img-fluid mb-2" style="max-height: 115px;">
+                        <p class="mb-0">{{ item.title }}</p>
+                    </div>
+
+                    <!-- Back -->
+                    <div :class="[flippedCards[key] ? 'd-block' : 'd-none', 'flip-back']">
+                        <p><strong>{{ item.title }}</strong></p>
+
+                        <p v-html="item.speciality" class="mb-0"></p>
+
+                        <div
+                            v-if="item.speciality"
+                            class="border-2 border-top mb-2 mt-2 mx-auto pb-1 w-75">
+                        </div>
+
+                        <div class="mb-1">
+                            <i class="bi bi-building"></i>
+                            <span class="ms-2">{{ item.school }}</span>
+                        </div>
+
+                        <div v-if="item.place" class="mb-1 text-secondary">
+                            <i class="bi bi-pin-angle"></i>
+                            <span class="ms-2">{{ item.place }}</span>
+                        </div>
+
+                        <div class="text-secondary">
+                            <i class="bi bi-calendar2-week"></i>
+                            <span class="ms-2">{{ item.date }}</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -32,6 +59,18 @@ export default {
         items: {
             type: Object,
             required: true
+        }
+    },
+
+    data() {
+        return {
+            flippedCards: {}
+        };
+    },
+
+    methods: {
+        toggleFlip(key) {
+            this.$set(this.flippedCards, key, !this.flippedCards[key]);
         }
     }
 };
