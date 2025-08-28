2221c4c59c25ca34195042833c011ec266607ca2
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-08-28 20:33:34 +0200

    style: minor changes in the footer

--- portfolio/src/components/FooterComponent.vue
+++ portfolio/src/components/FooterComponent.vue
@@ -1,35 +1,31 @@
 <template>
     <section id="contact" class="container social">
-        <div class="row">
-            <div class="col-12 text-center">
-                <ul class="list-inline">
-                    <li class="list-inline-item">
-                        <a :href="mail" target="_blank">
-                            <i class="bi bi-envelope-fill fs-4"></i>
-                        </a>
-                    </li>
+        <div class="text-center">
+            <ul class="list-inline">
+                <li class="list-inline-item">
+                    <a :href="mail" target="_blank">
+                        <i class="bi bi-envelope-fill fs-4"></i>
+                    </a>
+                </li>
 
-                    <li class="list-inline-item">
-                        <a :href="github" target="_blank">
-                            <i class="bi bi-github fs-4"></i>
-                        </a>
-                    </li>
+                <li class="list-inline-item">
+                    <a :href="github" target="_blank">
+                        <i class="bi bi-github fs-4"></i>
+                    </a>
+                </li>
 
-                    <li class="list-inline-item">
-                        <a :href="linkedin" target="_blank">
-                            <i class="bi bi-linkedin fs-4"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+                <li class="list-inline-item">
+                    <a :href="linkedin" target="_blank">
+                        <i class="bi bi-linkedin fs-4"></i>
+                    </a>
+                </li>
+            </ul>
         </div>
 
-        <div class="row">
-            <div class="col-12 text-center pt-2">
-                <p class="copyright">
-                    &copy; 2024 - {{ name }}
-                </p>
-            </div>
+        <div class="text-center mt-1">
+            <p class="copyright">
+                &copy; 2024 - {{ name }}
+            </p>
         </div>
     </section>
 </template>
