5ac03c5b89314ddf7b8cc7f60cedfef1ee1f8078
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2025-10-04 21:02:18 +0200

    chore: delete unused functions

--- portfolio/src/store/index.js
+++ portfolio/src/store/index.js
@@ -6,22 +6,16 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         language: JSON.parse(localStorage.getItem('language')) || "french",
-        projects: JSON.parse(localStorage.getItem('projects')) || null,
         project: JSON.parse(localStorage.getItem('project')) || null
     },
     getters: {
         getLanguage: state => state.language,
-        getProjects: state => state.projects,
         getProject: state => state.project
     },
     mutations: {
         setLanguage: function (state, language) {
             state.language = language;
             localStorage.setItem('language', JSON.stringify(language));
-        },
-        setProjects: function (state, projects) {
-            state.projects = projects;
-            localStorage.setItem('projects', JSON.stringify(projects));
         },
         setProject: function (state, project) {
             state.project = project;
